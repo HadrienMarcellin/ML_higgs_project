3cd63a16e6cd1496344ff9938926f46cfe932948
afrei <ana.frei@epfl.ch>
2018-10-13 17:05:36 +0200

    ml methods

---
+++ ML_methods.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""machine learning functions for project 1."""
+import numpy as np
+
+
+#####################################  --  MSE -- ###################################
+
+def calculate_mse(e):
+    return 0.5*np.mean(e**2)
+
+
+#####################################  --  MAE -- ###################################
+
+def calculate_mae(e):
+    return np.mean(np.abs(e))
+
+
+#####################################  --  LEAST SQUARES -- ###################################
+
+def least_squares(y, tx):
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    return np.linalg.solve(a,b)
+
+
+#####################################  --  GRADIENT -- ###################################
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+
+#####################################  --  GRADIENT DESCENT -- ###################################
+
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        grad, err = compute_gradient(y, tx, w)
+        loss = calculate_mse(err)
+        # gradient w by descent update
+        w = w - gamma * grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
+
+
+#####################################  --  STOCHASTIC GRADIENT DESCENT  -- ###################################
+# ACHTUNG : USED THE SAME GRADIENT CALCULATION AS FOR THE NORMAL GRADIENT DESCENT ....
+
+def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            # compute a stochastic gradient and loss
+            grad, _ = compute_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+            # calculate loss
+            loss = compute_loss(y, tx, w)
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
+
+
+#####################################  --  RIDGE REGRESSION  -- ###################################
+
+def ridge_regression(y, tx, lamb):
+    """implement ridge regression."""
+    aI = lamb * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
+
+
+
+
+
